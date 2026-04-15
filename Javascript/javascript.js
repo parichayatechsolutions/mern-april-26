58742203fba2fd7bb24eb17f10a5cad633c3c471
Mahalakshmi-2423 <mahalakshmimr3120@gmail.com>
2026-04-15 18:05:59 +0530

    js

---
+++ Javascript/javascript.js
@@ -0,0 +1,17 @@
+
+function myFunction(){
+    window.alert('Do not panic its a alert message');
+    document.getElementById("demo").innerHTML="Welcome to Javascript!";
+}
+function function1(){
+    document.getElementById("de").style.fontSize="35px";
+    document.getElementById("de").innerHTML="Font Changed";
+}
+function function2(){
+    document.getElementsById("sum").style.color="coral";
+    document.getElementsById("sum").innerHTML="The sum value"+c;
+    let a = 10;
+    let b = 25;
+    let c = b+a;
+}
+
